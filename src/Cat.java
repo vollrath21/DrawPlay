91b10ad75df0150302d255563b68e1fe56a6c762
Cameron Vollrath <vollrath@mines.edu>
2021-09-28 17:46:06 -0600

    Made the mouth wider

--- src/Cat.java
+++ src/Cat.java
@@ -17,7 +17,7 @@ public class Cat {
 	private static final int EYE_WIDTH = 100; // Made the eyes less realistic
 	// pick mouth height, width is based on head dimension
 	private static final int MOUTH_HEIGHT = 10;
-	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
+	private static final int MOUTH_WIDTH = HEAD_DIMENSION/2;
 	// mouth starts about 40% from left edge of head
 	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
 	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 3;
